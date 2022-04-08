1a4d1989471d9f0d688c5277026c961d1a4c6529
JodyGs <jody.gonzales@webingenierie.dev>
2022-04-08 09:19:56 +0200

    End of Header comp.

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMoralis } from "react-moralis";
 import Image from "next/image";
-import profileImg from "../public/profile.png";
+import logo from "../public/logo2.png";
 import Avatar from "./Avatar";
 import ChangeUsername from "./ChangeUsername";
 
@@ -9,18 +9,17 @@ function Header() {
   const { user, logout } = useMoralis();
 
   return (
-    <div className="text-fuchsia-600">
-      <div className="">
+    <div className="sticky top-0 p-5 z-50 bg-gradient-to-r from-indigo-800 to-fuchsia-600 shadoz-sm text-indigo-200 border-b-2 border-fuchsia-600">
+      <div className="grid grid-col-5 lg:grid-cols-6 items-end lg:items-center">
         <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
           <Image
             layout="fill"
             objectFit="cover"
-            className="rounded-full"
-            src={profileImg}
+            src={logo}
           />
         </div>
-        <div>
-          <div className="relative h-48 w-48 lg:mx-auto border border-fuchsia-600 border-8 rounded-full">
+        <div className="col-span-4 text-left lg:text-center">
+          <div className="relative h-48 w-48 lg:mx-auto border border-indigo-200 border-8 rounded-full">
               <Avatar logoutOnPress />
           </div>
           <h1 className="text-3xl">Welcome to Existenz</h1>
